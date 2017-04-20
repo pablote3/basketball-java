936cfff8802890294da18b63462464919b4f2096
Paul Rossotti <rossotti.paul@gmail.com>
2017-04-20 05:13:25 -0700

    initial commit

---
+++ accumulator/accumulator-loadSchedule/src/main/java/com/rossotti/basketball/AccumulatorLoadScheduleApplication.java
@@ -0,0 +1,12 @@
+package com.rossotti.basketball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccumulatorLoadScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccumulatorLoadScheduleApplication.class, args);
+	}
+}
