12ed520ae40a4dac25dce24d9068a7c95ab88298
Paul Rossotti <rossotti.paul@gmail.com>
2017-04-27 05:56:13 -0700

    build out reader with full select query

--- aggregator/src/main/java/com/rossotti/basketball/config/BatchConfig.java
+++ aggregator/src/main/java/com/rossotti/basketball/config/BatchConfig.java
@@ -57,26 +57,48 @@ public class BatchConfig {
     public Step stepTeam() {
         return stepBuilderFactory.get("stepTeam")
                 .<TeamBoxScore, TeamBoxScore>chunk(1)
-                .reader(teamReader())
+                .reader(teamBoxScoreReader())
                 .processor(teamProcessor())
                 .writer(teamFileWriter())
                 .build();
     }
 
     @Bean
-    public JdbcCursorItemReader<TeamBoxScore> teamReader() {
+    public JdbcCursorItemReader<TeamBoxScore> teamBoxScoreReader() {
         JdbcCursorItemReader<TeamBoxScore> reader = new JdbcCursorItemReader<>();
         //LocalDate gameDate = LocalDate.now().minusDays(1);
         LocalDate gameDate = LocalDate.of(2017, 3, 30);
         String minDateTime = DateTimeConverter.getStringDateTime(DateTimeConverter.getLocalDateTimeMin(gameDate));
         String maxDateTime = DateTimeConverter.getStringDateTime(DateTimeConverter.getLocalDateTimeMax(gameDate));
-        String sql = "select g.gameDateTime, t.teamKey, g.status " +
-                "from game g " +
-                "inner join boxScore as bs on g.id = bs.gameId " +
-                "inner join team as t on t.id = bs.teamId " +
-                "where g.gameDateTime between '" + minDateTime + "' and '" + maxDateTime + "' " +
-                "and bs.location = 'Home' " +
-                "order by g.gameDateTime asc";
+        String sql =
+            "SELECT game.gameDateTime, game.seasonType, " +
+                "team.abbr as teamAbbr, team.conference as teamConf, team.division as teamDiv, " +
+                "bsTeam.location as teamLocation, bsTeam.result as teamResult, bsTeam.minutes as teamMinutes, bsTeam.daysOff as teamDaysOff, " +
+                "bsTeam.points as teamPTS, bsTeam.assists as teamAST, bsTeam.turnovers as teamTOV, bsTeam.steals as teamSTL, bsTeam.blocks as teamBLK, bsTeam.personalFouls as teamPF, " +
+                "bsTeam.fieldGoalAttempts as teamFGA, bsTeam.fieldGoalMade as teamFGM, bsTeam.threePointAttempts as team3PA, bsTeam.threePointMade as team3PM, " +
+                "bsTeam.freeThrowAttempts as teamFTA, bsTeam.freeThrowMade as teamFTM, bsTeam.reboundsOffense as teamOREB, bsTeam.reboundsDefense as teamDREB, " +
+                "bsTeam.pointsPeriod1 as teamPTQ1, bsTeam.pointsPeriod2 as teamPTQ2, bsTeam.pointsPeriod3 as teamPTQ3, bsTeam.pointsPeriod4 as teamPTQ4, " +
+                "bsTeam.pointsPeriod5 as teamPTQ5, bsTeam.pointsPeriod6 as teamPTQ6, bsTeam.pointsPeriod7 as teamPTQ7, bsTeam.pointsPeriod8 as teamPTQ8, " +
+                "oppt.abbr as opptAbbr, oppt.conference as opptConf, oppt.division as opptDiv, " +
+                "bsOppt.location as opptLocation, bsOppt.result as opptResult, bsOppt.minutes as opptMinutes, bsOppt.daysOff as opptDaysOff, " +
+                "bsOppt.points as opptPTS, bsOppt.assists as opptAST, bsOppt.turnovers as opptTOV, bsOppt.steals as opptSTL, bsOppt.blocks as opptBLK, bsOppt.personalFouls as opptPF, " +
+                "bsOppt.fieldGoalAttempts as opptFGA, bsOppt.fieldGoalMade as opptFGM, bsOppt.threePointAttempts as oppt3PA, bsOppt.threePointMade as oppt3PM, " +
+                "bsOppt.freeThrowAttempts as opptFTA, bsOppt.freeThrowMade as opptFTM, bsOppt.reboundsOffense as opptOREB, bsOppt.reboundsDefense as opptDREB, " +
+                "bsOppt.pointsPeriod1 as opptPTQ1, bsOppt.pointsPeriod2 as opptPTQ2, bsOppt.pointsPeriod3 as opptPTQ3, bsOppt.pointsPeriod4 as opptPTQ4, " +
+                "bsOppt.pointsPeriod5 as opptPTQ5, bsOppt.pointsPeriod6 as opptPTQ6, bsOppt.pointsPeriod7 as opptPTQ7, bsOppt.pointsPeriod8 as opptPTQ8 " +
+            "FROM game AS game " +
+            "INNER JOIN boxScore AS bsTeam ON game.id = bsTeam.gameId " +
+            "INNER JOIN team AS team ON team.id = bsTeam.teamId " +
+            "INNER JOIN boxScore AS bsOppt ON game.id = bsOppt.gameId " +
+            "INNER JOIN team AS oppt ON oppt.id = bsOppt.teamId " +
+            "WHERE game.gameDateTime BETWEEN '" + minDateTime + "' AND '" + maxDateTime + "' " +
+            "AND bsTeam.location = 'Home' " +
+            "AND game.status = 'Completed' " +
+            "AND team.abbr <> oppt.abbr " +
+            "ORDER BY game.gameDateTime asc";
+
+        System.out.println("sql = " +sql);
+
         reader.setSql(sql);
         reader.setDataSource(databaseConfig.dataSource());
         reader.setRowMapper(new TeamBoxScoreMapper());
@@ -94,7 +116,7 @@ public class BatchConfig {
         writer.setResource(new FileSystemResource(new File("/home/pablote/pdrive/pwork/basketball/aggregator/extracts/paulOut.txt")));
         writer.setShouldDeleteIfExists(true);
         BeanWrapperFieldExtractor<TeamBoxScore> fieldExtractor = new BeanWrapperFieldExtractor<>();
-        fieldExtractor.setNames(new String[]{"gameDateTime", "teamKey", "status"});
+        fieldExtractor.setNames(new String[]{"gameDateTime", "seasonType", "teamAbbr", "teamConference", "teamDivision"});
         DelimitedLineAggregator<TeamBoxScore> lineAggregator = new DelimitedLineAggregator<>();
         lineAggregator.setFieldExtractor(fieldExtractor);
         writer.setLineAggregator(lineAggregator);
