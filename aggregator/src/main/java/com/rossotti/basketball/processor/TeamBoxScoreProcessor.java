e3b5555314d5276b9ca32f3fc9baa4596e76e31d
Paul Rossotti <rossotti.paul@gmail.com>
2017-04-29 09:31:42 -0700

    build initial calculators

--- aggregator/src/main/java/com/rossotti/basketball/processor/TeamBoxScoreProcessor.java
+++ aggregator/src/main/java/com/rossotti/basketball/processor/TeamBoxScoreProcessor.java
@@ -6,29 +6,48 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 
+import java.math.BigDecimal;
 
-public class TeamProcessor implements ItemProcessor<TeamBoxScore, TeamBoxScore> {
 
-    private final Logger logger = LoggerFactory.getLogger(TeamProcessor.class);
+public class TeamBoxScoreProcessor implements ItemProcessor<TeamBoxScore, TeamBoxScore> {
+
+    private final Logger logger = LoggerFactory.getLogger(TeamBoxScoreProcessor.class);
+    private TeamBoxScore teamBoxScore;
 
     @Override
-    public TeamBoxScore process(TeamBoxScore teamBoxScore) throws Exception {
+    public TeamBoxScore process(TeamBoxScore teamBoxScoreIn) {
+        teamBoxScore = teamBoxScoreIn;
         logger.info("TeamBoxScore: " + teamBoxScore.getTeamAbbr());
-
-        teamBoxScore.setPossessions(calculateTeamPossessions());
+        teamBoxScore.setPossessions(calculatePossessions().floatValue());
         teamBoxScore.setPace(calculatePace());
 
-        return teamBoxScore;
+        return this.teamBoxScore;
     }
 
-    private Float calculateTeamPossessions() {
-//      round(((bsTeam.fieldGoalAttempts - (bsTeam.reboundsOffense / (bsTeam.reboundsOffense + bsOppt.reboundsDefense)) * (bsTeam.fieldGoalAttempts - bsTeam.fieldGoalMade) * 1.07 + bsTeam.turnovers + (.4 * bsTeam.freeThrowAttempts)) +
-//             (bsOppt.fieldGoalAttempts - (bsOppt.reboundsOffense / (bsOppt.reboundsOffense + bsTeam.reboundsDefense)) * (bsOppt.fieldGoalAttempts - bsOppt.fieldGoalMade) * 1.07 + bsOppt.turnovers + (.4 * bsOppt.freeThrowAttempts))) /2, 4)
-        return 0F;
+    private Float calculatePossessions() {
+        BigDecimal bd = new BigDecimal(
+        (teamBoxScore.getTeamFieldGoalAttempts() - (teamBoxScore.getTeamReboundsOffense() / (teamBoxScore.getTeamReboundsOffense() + teamBoxScore.getOpptReboundsDefense())) * (teamBoxScore.getTeamFieldGoalAttempts() - teamBoxScore.getTeamFieldGoalMade()) * 1.07 + teamBoxScore.getTeamTurnovers() + (.4 * teamBoxScore.getTeamFreeThrowAttempts()))
+        +
+           (teamBoxScore.getOpptFieldGoalAttempts() - (teamBoxScore.getOpptReboundsOffense() / (teamBoxScore.getOpptReboundsOffense() + teamBoxScore.getTeamReboundsDefense())) * (teamBoxScore.getOpptFieldGoalAttempts() - teamBoxScore.getOpptFieldGoalMade()) * 1.07 + teamBoxScore.getOpptTurnovers() + (.4 * teamBoxScore.getOpptFreeThrowAttempts()))
+        /2
+        );
+
+        return bd.floatValue();
+    }
+
+    private Float calculatePossessions2() {
+        BigDecimal bd = new BigDecimal(teamBoxScore.getTeamFieldGoalAttempts())
+                .subtract((new BigDecimal(teamBoxScore.getTeamReboundsOffense())
+                        .divide(new BigDecimal(teamBoxScore.getTeamReboundsOffense() + teamBoxScore.getOpptReboundsDefense())))
+                        .multiply(new BigDecimal(teamBoxScore.getOpptFieldGoalAttempts() - teamBoxScore.getOpptFieldGoalMade())
+                                .multiply(new BigDecimal(1.07)))
+                        .add(new BigDecimal(teamBoxScore.getTeamTurnovers()))
+                        .add(new BigDecimal(.4 * teamBoxScore.getTeamFreeThrowAttempts())));
+        return bd.floatValue();
     }
 
     private Float calculatePace() {
-        return 0F;
+        return calculatePossessions() / (teamBoxScore.getTeamMinutes() * 48 * 5);
     }
 
     private Float calculateTeamOffEff() {
