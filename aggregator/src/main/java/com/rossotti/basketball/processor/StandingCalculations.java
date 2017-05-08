81014cbb8d3c2c41b0efc337257981a0cb519d4f
Paul Rossotti <rossotti.paul@gmail.com>
2017-05-07 21:01:15 -0700

    correct calculateStrengthOfSchedule

--- aggregator/src/main/java/com/rossotti/basketball/processor/StandingCalculations.java
+++ aggregator/src/main/java/com/rossotti/basketball/processor/StandingCalculations.java
@@ -8,13 +8,13 @@ class StandingCalculations {
     static BigDecimal calculateStrengthOfSchedule(Integer opptGamesWon, Integer opptGamesPlayed, Integer opptOpptGamesWon, Integer opptOpptGamesPlayed) {
         BigDecimal opptWinPct, opptOpptWinPct, strengthOfSchedule;
 
-        if (opptGamesPlayed > 0 && opptOpptGamesPlayed > 0) {
+        if (opptGamesPlayed > 0 && opptOpptGamesPlayed > 0 && opptOpptGamesWon > 0) {
             opptWinPct = new BigDecimal(opptGamesWon)
                 .divide(new BigDecimal(opptGamesPlayed), 4, RoundingMode.HALF_UP);
-            opptOpptWinPct = new BigDecimal(opptOpptGamesPlayed)
-                .divide(new BigDecimal(opptOpptGamesWon), 4, RoundingMode.HALF_UP);
+            opptOpptWinPct = new BigDecimal(opptOpptGamesWon)
+                .divide(new BigDecimal(opptOpptGamesPlayed), 4, RoundingMode.HALF_UP);
             strengthOfSchedule = (opptWinPct.multiply(new BigDecimal(2)).add(opptOpptWinPct))
-                .divide(new BigDecimal(2), 4, RoundingMode.HALF_UP);
+                .divide(new BigDecimal(3), 4, RoundingMode.HALF_UP);
         }
         else {
             strengthOfSchedule = new BigDecimal(0);
