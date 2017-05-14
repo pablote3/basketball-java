ff55cb3ff2763264872d6cac4280ab4fd1a089dc
Paul Rossotti <rossotti.paul@gmail.com>
2017-05-13 22:21:15 -0700

    initial commit

--- aggregator/aggregator-services/src/main/java/com/rossotti/basketball/config/DatabaseConfig.java
+++ aggregator/aggregator-services/src/main/java/com/rossotti/basketball/config/DatabaseConfig.java
@@ -1,12 +1,9 @@
 package com.rossotti.basketball.config;
 
-import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
-import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import javax.sql.DataSource;
 
@@ -20,17 +17,6 @@ public class DatabaseConfig {
         this.env = env;
     }
 
-    @Primary
-    @Bean
-    DataSource dataSourceAccumulate() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName(env.getRequiredProperty("db.accumulate.driver"));
-        dataSourceBuilder.url(env.getRequiredProperty("db.accumulate.url"));
-        dataSourceBuilder.username(env.getRequiredProperty("db.accumulate.username"));
-        dataSourceBuilder.password(env.getRequiredProperty("db.accumulate.password"));
-        return dataSourceBuilder.build();
-    }
-
     @Bean
     DataSource dataSourceAggregate() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
@@ -39,10 +25,5 @@ public class DatabaseConfig {
         dataSourceBuilder.username(env.getRequiredProperty("db.aggregate.username"));
         dataSourceBuilder.password(env.getRequiredProperty("db.aggregate.password"));
         return dataSourceBuilder.build();
-    }
-
-    @Bean
-    BatchConfigurer configurer (DataSource dataSource) {
-        return new DefaultBatchConfigurer(dataSource);
     }
 }
