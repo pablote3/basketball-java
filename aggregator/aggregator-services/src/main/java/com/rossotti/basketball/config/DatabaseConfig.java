cfbb7fdbad320dab57710a84f3eb27d7f041a60f
Paul Rossotti <rossotti.paul@gmail.com>
2017-05-14 07:59:26 -0700

    separate datasource for aggregate and accumulate

--- aggregator/aggregator-services/src/main/java/com/rossotti/basketball/config/DatabaseConfig.java
+++ aggregator/aggregator-services/src/main/java/com/rossotti/basketball/config/DatabaseConfig.java
@@ -1,9 +1,12 @@
 package com.rossotti.basketball.config;
 
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import javax.sql.DataSource;
 
@@ -17,13 +20,29 @@ public class DatabaseConfig {
         this.env = env;
     }
 
+    @Primary
     @Bean
-    DataSource dataSourceAggregate() {
+    public DataSource dataSourceAccumulate() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(env.getRequiredProperty("db.accumulate.driver"));
+        dataSourceBuilder.url(env.getRequiredProperty("db.accumulate.url"));
+        dataSourceBuilder.username(env.getRequiredProperty("db.accumulate.username"));
+        dataSourceBuilder.password(env.getRequiredProperty("db.accumulate.password"));
+        return dataSourceBuilder.build();
+    }
+
+    @Bean
+    public DataSource dataSourceAggregate() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName(env.getRequiredProperty("db.aggregate.driver"));
         dataSourceBuilder.url(env.getRequiredProperty("db.aggregate.url"));
         dataSourceBuilder.username(env.getRequiredProperty("db.aggregate.username"));
         dataSourceBuilder.password(env.getRequiredProperty("db.aggregate.password"));
         return dataSourceBuilder.build();
+    }
+
+    @Bean
+    BatchConfigurer configurer (DataSource dataSource) {
+        return new DefaultBatchConfigurer(dataSource);
     }
 }
