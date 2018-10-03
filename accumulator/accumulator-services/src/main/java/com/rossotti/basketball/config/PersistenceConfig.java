ecb4f74330e37f01be8ca67444e5bb315ccb708b
Paul Rossotti <rossotti.paul@gmail.com>
2018-10-02 17:57:14 -0700

    move PersistenceConfig to services

--- accumulator/accumulator-services/src/main/java/com/rossotti/basketball/config/PersistenceConfig.java
+++ accumulator/accumulator-services/src/main/java/com/rossotti/basketball/config/PersistenceConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -49,5 +51,12 @@ public class PersistenceConfig {
 
 		entityManagerFactoryBean.setJpaProperties(jpaProperties);
 		return entityManagerFactoryBean;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+		return transactionManager;
 	}
 }
