239be759bbedb0ac08c1c90acc64ab138ef90790
Paul Rossotti <rossotti.paul@gmail.com>
2018-10-03 21:52:22 -0700

    move integration classes to service project

---
+++ accumulator/accumulator-services/src/main/java/com/rossotti/basketball/config/IntegrationConfig.java
@@ -0,0 +1,81 @@
+package com.rossotti.basketball.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.scheduling.PollerMetadata;
+import org.springframework.messaging.MessageChannel;
+
+@Configuration
+@EnableIntegration
+@ComponentScan(basePackages = {"com.rossotti.basketball"})
+@IntegrationComponentScan(basePackages = {"com.rossotti.basketball.integration"})
+
+public class IntegrationConfig {
+    @Bean
+    public MessageChannel inputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel gameFinderChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel gameSplitterChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public QueueChannel gameRouterChannel() {
+        return new QueueChannel(20);
+    }
+
+    @Bean
+    public MessageChannel gameScoreChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel gameResultsChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel gameAggregatorChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel rosterLoadChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel standingsRouterChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel standingsRankChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel outputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean(name = PollerMetadata.DEFAULT_POLLER)
+    public PollerMetadata defaultPoller() {
+        PollerMetadata pollerMetadata = new PollerMetadata();
+        pollerMetadata.setMaxMessagesPerPoll(1L);
+ //       pollerMetadata.setTrigger(new PeriodicTrigger(10));
+        return pollerMetadata;
+    }
+}
